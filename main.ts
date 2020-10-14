da43e354ba141637f1c676b5c22cfffedc0c3864
ahit976 <55730608+ahit976@users.noreply.github.com>
2020-10-14 19:34:08 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -21,19 +21,19 @@ radio.onReceivedNumberDeprecated(function (receivedNumber) {
         maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
     }
     if (item == 5) {
-        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 64)
-        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 64)
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 32)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 32)
     }
     if (item == 6) {
-        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 64)
-        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 64)
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 32)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 32)
     }
     if (item == 7) {
         maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
-        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 32)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 16)
     }
     if (item == 8) {
-        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 32)
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 16)
         maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
     }
 })
