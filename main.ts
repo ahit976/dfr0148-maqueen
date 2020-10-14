4dde4577aa877421962c5873c1539c59b7da92ef
ahit976 <55730608+ahit976@users.noreply.github.com>
2020-10-14 19:31:52 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,41 @@
+radio.onReceivedNumberDeprecated(function (receivedNumber) {
+    item = receivedNumber
+    if (item == 0) {
+        maqueen.motorStop(maqueen.aMotors.M1)
+        maqueen.motorStop(maqueen.aMotors.M2)
+    }
+    if (item == 1) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
+    }
+    if (item == 2) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 255)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 255)
+    }
+    if (item == 3) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
+    }
+    if (item == 4) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
+    }
+    if (item == 5) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 64)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 64)
+    }
+    if (item == 6) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 64)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 64)
+    }
+    if (item == 7) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 32)
+    }
+    if (item == 8) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 32)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
+    }
+})
+let item = 0
+radio.setGroup(1)
